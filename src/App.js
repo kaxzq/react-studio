d798dc01acf81eb907501748118a84e3622bd27b
kaxzq <zhaoqi_xu@brown.edu>
2022-11-11 08:47:40 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,20 +10,28 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+
+console.log(cart);
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1 className="App-h1">Zhaoqi's Bakery</h1>
+      <div className="page">
+      <div className="App-container">
+      {bakeryData.map((item, index) => (
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem key={index} bakery={item} image={item.image} count={item.count} setCart={setCart} cart={cart} total={total} setTotal={setTotal}></BakeryItem>
+ 
       ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      </div>
+    <div>
+        <h2 className="cart-h2">Cart</h2>
+        <div>{cart.map((item, index) => (<h5 className="item-h5" key={index}>{item.count} x {item.name}</h5>))}</div>
+        <div><h4 className="total-h4">Total: ${total.toFixed(2)}</h4></div>
+      </div>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,34 @@
-// TODO: create a component that displays a single bakery item
+import "../App.css";
+export default function BakeryItem(props) {
+  return (
+    <div className="BakeryItem">
+      <img className="Bakery-img" src={props.image} alt={props.bakery.name}></img>
+      <h1 className="Bakery-h1">{props.bakery.name}</h1>
+      <h2 className="Bakery-h2">{props.bakery.description}</h2>
+      <div className="Price-container">
+        <h3 className="Bakery-h3">${props.bakery.price}</h3>
+        <AddButton />
+      </div>
+    </div>
+  );
+  function AddButton() {
+    function handleClick() {
+      let newCart = props.cart;
+      let i = newCart.findIndex(item => item.name == props.bakery.name);  
+      if (i == -1) {
+        props.setCart([...props.cart, {name: props.bakery.name, count: 1}]);
+      } else {
+        const c = newCart[i].count;
+        newCart[i] = {name: props.bakery.name, count: c+1};
+        props.setCart([...newCart]);
+      }
+      props.setTotal(props.total + props.bakery.price);
+    }
+    return (
+       <button className="Bakery-button" onClick={handleClick}>Add to Cart</button>
+    );
+  }
+}
+
+
+
